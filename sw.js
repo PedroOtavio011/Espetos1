5667810ae01d18e0cf2d68baa029d0b468f6ec74
PedroOtavio011 <pedrodozza2018@gmail.com>
2026-06-08 18:44:01 -0300

    Implementação do PWA

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'espetos-v0';
+const assets = [
+    './index.html',
+    './dashboard.html',
+    './dashBoardFunc.html',
+    './server/dashBoard.js',
+    './server/bipar.js',
+    './server/login.js',
+    './server/navigation.js',
+    './css/style.css',
+    './css/dashBoard.css',
+    './css/dashBoardFunc.css'
+];
+
+// Instala o Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Executa as requisições
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
+});
